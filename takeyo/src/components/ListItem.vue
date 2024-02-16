eb2b57544e969701a21a65a1d32096f92a140cee
takeyodesu <takeyodesu@gmail.com>
2024-02-16 05:09:00 +0000

    Updates

---
+++ takeyo/src/components/ListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="image" alt="" />
+
+  <div class="p-listItem__title">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    image: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {},
+};
+</script>
