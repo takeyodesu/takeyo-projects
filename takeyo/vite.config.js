5dcfff6827be09b463af84ec935a91426c0dc36d
takeyodesu <takeyodesu@gmail.com>
2024-02-16 17:14:05 +0900

    遷移style修正再deploy

--- takeyo/vite.config.js
+++ takeyo/vite.config.js
@@ -2,20 +2,27 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    return {
-        plugins: [vue()],
-        resolve: {
-            extensions: [".vue", ".js", ".scss"],
-            alias: {
-                "@": fileURLToPath(new URL("./src", import.meta.url)),
-            },
+  return {
+    plugins: [vue()],
+    resolve: {
+      extensions: [".vue", ".js", ".scss"],
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    build: {
+      outDir: "./dist",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          404: resolve(__dirname, "404.html"),
         },
-        build: {
-            outDir: "./dist",
-        },
-        base: mode === "development" ? "./" : "/takeyo-projects/",
-    }
+      },
+    },
+    base: mode === "development" ? "./" : "/takeyo-projects/",
+  };
 });
