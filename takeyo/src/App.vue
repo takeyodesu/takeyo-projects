893f4a5adaf603e4970af414306e454cbdabdfcd
takeyodesu <takeyodesu@gmail.com>
2024-07-02 22:51:11 +0900

    [vue,scss]RouterLinkデザイン修正

--- takeyo/src/App.vue
+++ takeyo/src/App.vue
@@ -1,33 +1,54 @@
 <template>
-  <main class="l-content">
+  <main>
     <div class="p-nav-container">
-      <div class="p-nav">
-        <RouterLink to="/">TOP</RouterLink>
-        <RouterLink to="/web">WEBアプリ</RouterLink>
-        <RouterLink to="/shikikakuijou">色覚異常</RouterLink>
-        <RouterLink to="/poster">映画ポスター</RouterLink>
-        <RouterLink to="/graphic">グラフィック</RouterLink>
-        <RouterLink to="/lp">動物病院LP</RouterLink>
+      <div class="p-navBtn" @click="eventClickOpenNav">
+        <img :src="getImageUrl('IconHamMenuWhite.svg')" />
+      </div>
+      <div v-if="showOpenNav" class="p-nav">
+        <RouterLink to="/" @click="closeNav">TOP</RouterLink>
+        <RouterLink to="/web" @click="closeNav">WEBアプリ</RouterLink>
+        <RouterLink to="/shikikakuijou" @click="closeNav">色覚異常</RouterLink>
+        <RouterLink to="/poster" @click="closeNav">映画ポスター</RouterLink>
+        <RouterLink to="/graphic" @click="closeNav">グラフィック</RouterLink>
+        <RouterLink to="/lp" @click="closeNav">動物病院LP</RouterLink>
       </div>
     </div>
-    <RouterView />
+    <div class="l-content">
+      <RouterView />
+    </div>
   </main>
 </template>
 
 <script>
 import { RouterLink, RouterView } from "vue-router";
-import ListItem from "./components/ListItem.vue";
-import About from "./components/About.vue";
 import { ref, computed } from "vue";
+
 export default {
   name: "TopIndex",
-  components: {},
-  computed: {
-    getImageUrl: () => {
-      return (name) => {
-        return new URL(`./images/${name}`, import.meta.url).href;
-      };
-    },
+  components: {
+    RouterLink,
+    RouterView,
+  },
+  setup() {
+    const showOpenNav = ref(false);
+
+    const getImageUrl = (name) => {
+      return new URL(`./images/${name}`, import.meta.url).href;
+    };
+
+    const eventClickOpenNav = () => {
+      showOpenNav.value = !showOpenNav.value;
+    };
+    const closeNav = () => {
+      showOpenNav.value = false;
+    };
+
+    return {
+      showOpenNav,
+      getImageUrl,
+      eventClickOpenNav,
+      closeNav,
+    };
   },
 };
 </script>
